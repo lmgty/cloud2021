a241b10229ea3d6dc55efb5593ef27161d47277c
YeLiu <1063042553@qq.com>
2021-04-16 16:27:27 +0800

    注册 order80、payment8001 到注册中心

--- cloud-consumer-order80/src/main/java/com/yufa/xz/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/yufa/xz/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.yufa.xz.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author LiuYe
  * @data 2021/3/23
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
