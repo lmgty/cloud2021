9270828593edac147f3e7ff1ac82473658720f58
YeLiu <1063042553@qq.com>
2021-04-16 17:46:55 +0800

    配置 Eureka 集群

---
+++ cloud-eureka-server7002/src/main/java/com/yufa/xz/springcloud/EurekaMain7002.java
@@ -0,0 +1,17 @@
+package com.yufa.xz.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author LiuYe
+ * @data 2021/4/16
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
